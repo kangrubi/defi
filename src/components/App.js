941c12b6e46ae54bd5a21d4438a9c0275f5de4b6
kangrubi <rubi0624@nate.com>
2024-05-22 18:25:29 +0900

    ✨ (App.js): DaiToken, dappToken, tokenFarm, daiTokenBalance, dappTokenBalance, stakingBalance, loading 상태 추가 📝 (App.js): loadWeb3 함수에 account 및 networkId 설정 추가 📝 (App.js): loadWeb3 함수에 daiToken 객체 생성 및 상태 업데이트 추가 📝 (App.js): loadDaiTokenBalance 함수에 DaiToken 잔액 조회 로직 추가 📝 (App.js): useEffect 훅을 이용하여 DaiToken 잔액 조회 로직 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import Web3 from "web3";
+import DaiToken from "../abis/DaiToken.json";
 
 const App = () => {
   const [state, setState] = useState({
     account: "0x0",
+    daiToken: {},
+    dappToken: {},
+    tokenFarm: {},
+    daiTokenBalance: "0",
+    dappTokenBalance: "0",
+    stakingBalance: "0",
+    loading: true,
   });
 
   const loadWeb3 = async () => {
@@ -33,13 +41,52 @@ const App = () => {
 
     const accounts = await web3.eth.getAccounts();
 
-    console.log(accounts);
+    setState((prevState) => ({ ...prevState, account: accounts[0] }));
+
+    const networkId = await web3.eth.getChainId();
+
+    // 컨트랙트 객체 생성
+    const daiTokenData = DaiToken.networks[networkId];
+    if (daiTokenData) {
+      const daiToken = new web3.eth.Contract(
+        DaiToken.abi,
+        daiTokenData.address
+      );
+
+      setState((prevState) => ({
+        ...prevState,
+        daiToken,
+      }));
+    }
+  };
+
+  const loadDaiTokenBalance = async () => {
+    const { account, daiToken } = state;
+
+    // 컨트랙트 객체의 메서드 호출
+    if (daiToken && account) {
+      try {
+        const daiTokenBalance = await daiToken.methods
+          .balanceOf(account)
+          .call();
+
+        console.log(daiTokenBalance);
+      } catch {
+        console.error("Error fetching DaiToken balance:");
+      }
+    }
   };
 
   useEffect(() => {
     loadWeb3();
     loadBlockchainData();
   }, []);
+
+  useEffect(() => {
+    if (state.daiToken && state.account !== "0x0") {
+      loadDaiTokenBalance();
+    }
+  }, [state.account, state.daiToken]);
 
   return (
     <div>
