f57b61e57d8a25c9e1b9130260b162d2afbad81c
kangrubi <rubi0624@nate.com>
2024-05-23 16:46:27 +0900

    ✨ (Navbar.js): Update Navbar component to display the user's account information in the navigation bar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,16 +17,9 @@ const Navbar = ({ account }) => {
           className="d-inline-block align-top"
           alt=""
         />
-        &nbsp; DApp Token Farm
+        &nbsp; DApp Token Farm &nbsp;
+        {account}
       </a>
-
-      <ul className="navbar-nav px-3">
-        <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-          <small className="text-secondary">
-            <small id="account">{account}</small>
-          </small>
-        </li>
-      </ul>
     </nav>
   );
 };
